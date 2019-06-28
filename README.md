fed1103b01f95c8b688b4a7b0fea4217a98a38db
sty <scarsty@gmail.com>
2019-06-28 13:48:37 +0800

    。。。。。。。。。。

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-Instructions for using Artistic Style are included in the *doc* directory.
-
-The file **install.html** contains instructions for compiling and
-installing Artistic Style.
-
-The file **astyle.html**' contains information on using Artistic Style.
-
-The files **news.html** and **notes.html** contain information on changes
-made to the various releases.
+自用的格式化代码
---
+++ format-test.cpp
@@ -0,0 +1,19 @@
+#define WAIT_UNTIL(condition) {{i=0;} }
+#define WAIT_UNTIL_OVERTIME(condition, overtime) \
+        { \
+   {} }
+{
+    auto zoomOneDefect = []()
+    {
+    };}
+    int a=int{0};
+        void setLearnRateBase(real lrb)
+                      {
+        for (auto& n : nets_)        {            n->setLearnRateBase(lrb);
+        }
+    }
+
+    //4阶张量形式
+Matrix::Matrix(int w, int h, int c, int n, MatrixDataType data_type, DeviceType device_type) : Matrix({ w, h, c, n }, data_type, device_type)
+{
+}
