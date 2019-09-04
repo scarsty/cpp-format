e0ba35edef04872cf387fdd99dee57f313546ec8
sty <scarsty@gmail.com>
2019-09-04 13:00:37 +0800

    测试用例

--- format-test.cpp
+++ format-test.cpp
@@ -17,3 +17,38 @@
 Matrix::Matrix(int w, int h, int c, int n, MatrixDataType data_type, DeviceType device_type) : Matrix({ w, h, c, n }, data_type, device_type)
 {
 }
+void zoomResult()
+{
+    std::function<void(DefectRect&)> zoomOneDefect = [&](DefectRect& r)
+{};}
+auto c=[](int& i)->int
+{
+};
+
+#define dfdfdas auto c=[](int& i)->int\
+{\
+};
+
+#define dfdfdas           \
+auto c=[](int& i)->int\
+{\
+};
+
+void zoomResult()
+{
+    std::function<void(Rect& r)> zoomOne = [&](Rect& r)    //todo: clang-format not right
+    {
+        r.x /= zoom_;
+        r.y /= zoom_;
+        r.width /= zoom_;
+        r.height /= zoom_;
+        if (r.segmentation.cols > 0 && r.segmentation.rows > 0)
+        {
+            cv::resize(r.segmentation, r.segmentation, cv::Size(r.width, r.height), 0, 0, CV_INTER_NN);
+        }
+        for (auto& rs : r.sub_defects)
+        {
+            zoomOneDefect(rs);
+        }
+    };
+}
